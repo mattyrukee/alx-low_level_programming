e4f43a17033b8e128a971dc7837ae050de794110
Mattyrukee <mattyrukee19@gmail.com>
2022-09-12 05:54:30 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
 * main - print the string in the put function
-*
 *Discription: using the main function
 *This program prints "Programming is like building a multilingual puzzle
 *Return: 0
