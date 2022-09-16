722d5462658b3e731791fabd55e2a9455ea28f3e
Mattyrukee <mattyrukee19@gmail.com>
2022-09-15 18:52:06 -0700

    Error found

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print-remaining_days - takes a date and prints how many days are
+ * print_remaining_days - takes a date and prints how many days are
  * left int the year, taking leap years into account
  * @month: month is number of format
  * @day: day of month
@@ -25,12 +25,12 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: % 02d/02d/ %04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the year : % d\n", day);
-			printf("Remaining days : % d\n", 365 - day);
+			printf("Day of the year :%d\n", day);
+			printf("Remaining days :%d\n", 365 - day);
 		}
 	}
 }
