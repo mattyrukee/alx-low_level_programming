81391ff83a2b992d7270587094e2cbe5536497b8
Mattyrukee <mattyrukee19@gmail.com>
2022-09-22 06:17:06 -0700

    go

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,13 +6,14 @@
  * @n: number of elements
  */
 
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int tmp, index;
 
 	for (index = n - 1; index > n / 2; index--)
 	{
 		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
 }
